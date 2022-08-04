7d598b841fa6ae25bda538c99ec088eddc93e984
Z9tails <100103488+Z9tails@users.noreply.github.com>
2022-08-04 15:00:04 -0700

    Added port

--- config/connection.js
+++ config/connection.js
@@ -9,7 +9,7 @@ const sequelize = process.env.JAWSDB_URL
   : new Sequelize(process.env.DB_name, process.env.DB_user, process.env.DB_password, {
       host: "localhost",
       dialect: "mysql",
-      port: 3300,
+      port: 3306,
     });
 
 module.exports = sequelize;
